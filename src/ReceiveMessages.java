7643cf470b71e4b40aca5424e00a7228f41f62d6
ejdarcy <emmadarcy27@gmail.com>
2017-11-21 17:19:59 +0000

    Added a new method called checkForBoughtShares() which will check for confirmation of a purchase of shares and will store the stock name and price along with the amount of shares in a dictionary.

--- src/ReceiveMessages.java
+++ src/ReceiveMessages.java
@@ -3,10 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReceiveMessages implements Runnable {
 	public Socket clientSocket = null; 
 	public BufferedReader in = null;
+	public static Map<String, String> boughtShares = new HashMap<String, String>();
 	
 	public ReceiveMessages(Socket client) throws IOException
 	{
@@ -14,20 +17,43 @@ public class ReceiveMessages implements Runnable {
 		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	}
 	
+	public Map<String, String> getBoughtShares()
+	{
+		return boughtShares;
+	}
+	
 	public void PrintMessage() throws IOException
 	{
-		System.out.println("Server: " + in.readLine());
+		
+		String str1 = in.readLine();
+		
+		System.out.println("Server: " + str1);
+		
+		if(str1.contains("BOUGHT"))
+			checkForBoughtShares(str1);
 	}
 
 	@Override
-	public void run() {
+	public void run() 
+	{
 		while(true)
 		{
-			try {
+			try {				
 				PrintMessage();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}	
+	}
+	
+	public void checkForBoughtShares(String result) throws IOException
+	{
+		
+		String[] tokens = result.split(":"); 
+		String stockName = tokens[3].split(" ")[1];
+		String stockAmount = tokens[2].split(" ")[0];
+		boughtShares.put(stockName.concat(tokens[4]), stockAmount);		
+		System.out.println(stockAmount + " shares in " + stockName.concat(tokens[4]));
+
 	}
 }
